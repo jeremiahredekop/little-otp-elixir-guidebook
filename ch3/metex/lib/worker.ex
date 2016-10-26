9287bc7de903f9e5d0b1f3f3ad78d15646ca0d22
Jeremiah Redekop <Jeremiah@geniuscode.net>
2016-10-26 08:46:47 -0700

    working genServer

--- ch3/metex/lib/worker.ex
+++ ch3/metex/lib/worker.ex
@@ -1,23 +1,71 @@
 defmodule Metex.Worker do
 
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
-        _ ->
-        IO.puts "don't know how to process this message"
-    end
-    loop
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  def temperature_of(location) do
-    result = url_for(location) |> HTTPoison.get |> parse_response
-    case result do
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  def get_stats(pid) do
+    GenServer.call(pid, :get_stats)
+  end
+
+  def reset_stats(pid) do
+    GenServer.cast(pid, :reset_stats)
+  end
+
+  def stop(pid) do
+    GenServer.cast(pid, :stop)
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def terminate(reason, stats) do
+    # We could write to a file, db, etc
+    IO.puts "server terminated because of #{inspect reason}"
+      inspect stats
+    :ok
+  end
+
+  ## Server API
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
       {:ok, temp} ->
-        "#{location}: #{temp}°C"
-      :error ->
-        "#{location} not found"
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats}
+      _ ->
+        {:reply, :error, stats}
     end
+  end
+
+  def handle_call(:get_stats, _from, stats) do
+    {:reply, stats, stats}
+  end
+
+  def handle_cast(:reset_stats, _stats) do
+    {:noreply, %{}}
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats}
+  end
+
+  ## Helper Functions
+
+
+
+
+  def temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_response
   end
 
   defp url_for(location) do
@@ -44,6 +92,17 @@ defmodule Metex.Worker do
 
   defp apikey do
     "8a69b02e04cfd62e2bf3217e3b5d8d8c"
+  end
+
+  defp update_stats(old_stats, location) do
+
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+
   end
 
 end
